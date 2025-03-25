6e07184331d4ec56995dd9706da1a3618569fc82
Jose Galeana <kevingaleana@yahoo.com>
2025-03-24 22:47:02 -0500

    all done

--- src/EpisodeListItem.jsx
+++ src/EpisodeListItem.jsx
@@ -1,15 +1,15 @@
 export default function EpisodeListItem({
-    episode,
-    selectedEpisode,
-    setSelectedEpisode,
-  }) {
-    return (
-      <li
-        key={episode.id}
-        onClick={() => setSelectedEpisode(episode)}
-        className={selectedEpisode?.id === episode.id ? "selected" : ""}
-      >
-        {episode.title}
-      </li>
-    );
-  }
+  episode,
+  selectedEpisode,
+  setSelectedEpisode,
+}) {
+  return (
+    <li
+      key={episode.id}
+      onClick={() => setSelectedEpisode(episode)}
+      className={selectedEpisode?.id === episode.id ? "selected" : ""}
+    >
+      {episode.title}
+    </li>
+  );
+}
